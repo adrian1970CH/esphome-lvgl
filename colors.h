71a143f80eac7dc37422333cf0182de047eafb14
adrian1970CH <adrian.chelaru1970@gmail.com>
2025-08-04 18:33:39 +0300

    Add files via upload

---
+++ colors.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "esphome.h"
+
+// Color definitions for ESPHome LVGL
+#define WHITE_HEX 0xFFFFFF
+#define BLACK_HEX 0x000000
+#define BRIGHT_ORANGE_HEX 0xFA6D21
+#define DARK_ORANGE_HEX 0x884019
+#define BRIGHT_BLUE_HEX 0x2196F3
+#define DARK_BLUE_HEX 0x195485
+#define DARK_GRAY_HEX 0x292929
+#define LIGHT_GRAY_HEX 0x848484
+#define NEUTRAL_GRAY_HEX 0x565656
+#define AMBER_HEX 0xFFC107
+
+// Color constants
+static const lv_color_t WHITE = lv_color_hex(WHITE_HEX);
+static const lv_color_t BLACK = lv_color_hex(BLACK_HEX);
+static const lv_color_t BRIGHT_ORANGE = lv_color_hex(BRIGHT_ORANGE_HEX);
+static const lv_color_t DARK_ORANGE = lv_color_hex(DARK_ORANGE_HEX);
+static const lv_color_t BRIGHT_BLUE = lv_color_hex(BRIGHT_BLUE_HEX);
+static const lv_color_t DARK_BLUE = lv_color_hex(DARK_BLUE_HEX);
+static const lv_color_t DARK_GRAY = lv_color_hex(DARK_GRAY_HEX);
+static const lv_color_t LIGHT_GRAY = lv_color_hex(LIGHT_GRAY_HEX);
+static const lv_color_t NEUTRAL_GRAY = lv_color_hex(NEUTRAL_GRAY_HEX);
+static const lv_color_t AMBER = lv_color_hex(AMBER_HEX);
